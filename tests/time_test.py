e49bae2e09c147d080ddf31d2b1746deb9066795
felsiq <felipe.siqueira@usp.br>
2023-09-10 11:57:41 -0300

    add: time test

--- tests/time_test.py
+++ tests/time_test.py
@@ -9,18 +9,22 @@ import dbcv
 
 
 def compute(n_instances: int, n_dims: int, n_clusters: int) -> None:
-    X = np.random.randn(n_instances, n_dims)
-    y = np.random.randint(0, n_clusters, size=n_instances)
-    score = dbcv.dbcv(X, y)
+    rng = np.random.RandomState(1892)
+    X = rng.randn(n_instances, n_dims)
+    y = rng.randint(0, n_clusters, size=n_instances)
+    score = dbcv.dbcv(X, y, n_processes=8)
     assert -1.0 <= score <= 1.0, score
 
 
 def test():
-    combs = itertools.product([100, 1000, 10000], [10, 100, 1000], [2, 4, 8, 16])
+    combs = itertools.product([100, 1000, 5000], [10, 100, 150], [2, 4, 8, 16])
     results = []
-    for args in tqdm.tqdm(combs, total=3 * 3 * 4):
+    pbar = tqdm.tqdm(combs, total=3 * 3 * 4)
+
+    for args in pbar:
         fn = functools.partial(compute, *args)
-        min_time = min(timeit.repeat(fn, repeat=3, number=20))
+        pbar.set_description(str(args))
+        min_time = min(timeit.repeat(fn, repeat=2, number=10))
         results.append((args, min_time))
 
     for item in results:
